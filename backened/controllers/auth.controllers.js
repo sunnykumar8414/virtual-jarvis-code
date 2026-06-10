f8cccc934bb29c4148d0600c6086c60562390813
sunnykumar8414 <sunnykumar697798@gmail.com>
2026-06-10 11:21:47 +0530

    Update auth.controllers.js

--- backened/controllers/auth.controllers.js
+++ backened/controllers/auth.controllers.js
@@ -24,8 +24,8 @@ export const signUp = async (req , res) => {
         res.cookie("token" , token ,{
             httpOnly : true,
             maxAge : 7*24*60*60*1000,
-            sameSite : "strict",
-            secure : false
+            sameSite : "None",
+            secure : true
         })
 
         return res.status(200).json(user)
@@ -58,8 +58,8 @@ export const Login = async (req , res) => {
         res.cookie("token" , token ,{
             httpOnly : true,
             maxAge : 7*24*60*60*1000,
-            sameSite : "strict",
-            secure : false
+            sameSite : "None",
+            secure : true
         })
 
         return res.status(200).json(user)
